caa3fa22e777e589a748b3a74ee53c2ae8aebbbb
theredwillow <theredwillows@gmail.com>
2017-09-25 08:08:35 -0500

    Overhaul Pt.2
    Updating the script to reflect the changes I've made over the weekends

--- background.js
+++ background.js
@@ -18,69 +18,90 @@ chrome.runtime.onMessage.addListener(
 
 	    // console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension", request);
 
-	    // If content.js is asking if menu is already open
-	    if (request.msg == "ask_edit") {
-	    	// console.log("Checking to see if there is an edit for tab id", sender.tab.id);
-	    	var respondWith = false;
-			for (var index in websitesInEdit) {
-				if (index == sender.tab.id) {
-				    respondWith = {};
-				    respondWith.x = websitesInEdit[sender.tab.id].x;
-				    respondWith.y = websitesInEdit[sender.tab.id].y;
-				    respondWith.title = websitesInEdit[sender.tab.id].title || "Untitled Scraper";
-				    respondWith.page = websitesInEdit[sender.tab.id].page;
-				    break;
+	    switch (request.msg) {
+
+		    case "ask_edit": // If content.js is asking if menu is already open
+		    	// console.log("Checking to see if there is an edit for tab id", sender.tab.id);
+		    	var respondWith = false;
+				for (var index in websitesInEdit) {
+					if (index == sender.tab.id) {
+					    respondWith = {};
+					    respondWith.x = websitesInEdit[sender.tab.id].x;
+					    respondWith.y = websitesInEdit[sender.tab.id].y;
+					    respondWith.title = websitesInEdit[sender.tab.id].title || "Untitled Scraper";
+					    respondWith.page = websitesInEdit[sender.tab.id].page;
+					    respondWith.details = websitesInEdit[sender.tab.id].details;
+					    break;
+					}
 				}
-			}
-	    	sendResponse({"msg": "respond_edit", "tabInEdit": respondWith});
-	    }
+		    	sendResponse({"msg": "respond_edit", "tabInEdit": respondWith});
+		        break;
 
-	    // If the message has the starting dom, add it to websitesInEdit
-	    else if (request.msg == "start") {
-			websitesInEdit[sender.tab.id] = {
-				[sender.tab.url] : {
-					"dom" : {
-						[Date.now()] : request.dom
+		    case "start":  // If the message has the starting dom, add it to websitesInEdit
+				websitesInEdit[sender.tab.id] = {
+					[sender.tab.url]: {
+						"dom" : {
+							[Date.now()] : request.dom
+						},
+						"click": {},
+						"selectors": {},
+						"origin": true
 					},
-					"click" : { },
-					"origin" : true
-				},
-				"page": "title",
-				"x" : "0px",
-				"y" : "0px"
-			};
-			console.log("websitesInEdit is now:", websitesInEdit);
-	    }
+					"page": "title",
+					"details": {},
+					"x": "0px",
+					"y": "0px"
+				};
+				chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+					chrome.tabs.sendMessage(tabs[0].id, {"msg": "change_page", "page": "title"});
+				});
+				console.log("websitesInEdit is now:", websitesInEdit);
+		        break;
 
-	    else if (request.msg == "move_popup") {
-	    	console.log("Window in tab", sender.tab.id, "moved to x:", request.x, "y", request.y);
-	    	websitesInEdit[sender.tab.id].x = request.x;
-	    	websitesInEdit[sender.tab.id].y = request.y;
-	    }
+		    case "move_popup":
+		    	console.log("Window in tab", sender.tab.id, "moved to x:", request.x, "y", request.y);
+		    	websitesInEdit[sender.tab.id].x = request.x;
+		    	websitesInEdit[sender.tab.id].y = request.y;
+		    	break;
 
-	    else if (request.msg == "add_url" && !(sender.tab.url in websitesInEdit[sender.tab.id]) ) {
-			websitesInEdit[sender.tab.id][sender.tab.url] = {
-				"dom" : { [Date.now()] : request.dom },
-				"click" : { },
-				"origin" : false
-			};
-	    }
+		    case "add_url":
+			    if ( !(sender.tab.url in websitesInEdit[sender.tab.id]) ) {
+					websitesInEdit[sender.tab.id][sender.tab.url] = {
+						"dom": { [Date.now()] : request.dom },
+						"click": {},
+						"selectors": {},
+						"origin": false
+					};
+			    }
+		    	break;
 
-	    else if (request.msg == "pass_click") {
-	    	websitesInEdit[sender.tab.id][sender.tab.url]["click"][Date.now()] = request.element; 
-	    }
+		    case "pass_click":
+		    	websitesInEdit[sender.tab.id][sender.tab.url]["click"][Date.now()] = request.element;
+		    	break;
 
-	    else if (request.msg == "pass_dom") {
-	    	websitesInEdit[sender.tab.id][sender.tab.url]["dom"][Date.now()] = request.dom; 
-	    }
+		    case "pass_dom":
+		    	websitesInEdit[sender.tab.id][sender.tab.url]["dom"][Date.now()] = request.dom;
+		    	break;
 
-	    else if (request.msg == "change_title") {
-	    	websitesInEdit[sender.tab.id].title = request.title; 
-	    }
+		    case "change_title":
+		    	websitesInEdit[sender.tab.id].title = request.title;
+		    	if ( websitesInEdit[sender.tab.id].page == "title" ) {
+					chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+						chrome.tabs.sendMessage(tabs[0].id, {"msg": "change_page", "page": "main"});
+					});
+				}
+		    	break;
 
-	    else if (request.msg == "remove_scraper") {
-	    	delete websitesInEdit[sender.tab.id]; 
-	    }
+		    case "record_pageChange":
+		    	websitesInEdit[sender.tab.id].page = request.page;
+		    	websitesInEdit[sender.tab.id].details = request.details;
+		    	break;
+
+		    case "remove_scraper":
+		    	delete websitesInEdit[sender.tab.id];
+		    	break;
+
+		}
 
 	}
 );
